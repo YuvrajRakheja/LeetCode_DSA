ac4374a762c0bd1508e54410793d4b84312e06b4
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2022-12-06 23:15:06 +0530

    Time: 1 ms (100.00%), Space: 44.2 MB (88.00%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,5 +1,5 @@
-//TC-0(KNLOG(N))
-//SC-0(K) MAX IN PQ
+//TC-0(NKLOGN(K))
+//SC-0(1)
 
 /**
  * Definition for singly-linked list.
@@ -13,30 +13,41 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->{
-                                        return a.val-b.val;});//this-other for default behaviour
-                                    //return b.val-a.val;})//other-this for reverse of default behaviour
-        
-        for(ListNode l:lists){
-            if(l!=null){
-                pq.add(l);
-            }
+        if(lists.length==0){
+            return null;
         }
+        return mergeKLists(0,lists.length-1,lists);
+    }
+    public ListNode mergeKLists(int si,int li,ListNode[] lists){
+        if(si>li)   return null;
+        if(si==li)  return lists[si];
         
+        int mid=(si+li)/2;
+        
+        ListNode fsh=mergeKLists(si,mid,lists);
+        ListNode ssh=mergeKLists(mid+1,li,lists);
+        
+        return merge2LL(fsh,ssh);
+    }
+    public ListNode merge2LL(ListNode l1,ListNode l2){
+        if(l1==null || l2==null){
+            return l1!=null?l1:l2;
+        }
         ListNode dummy=new ListNode(-1);
-        ListNode prev=dummy;
+        ListNode previous=dummy;
+        ListNode c1=l1,c2=l2;
         
-        while(pq.size()>0){
-            ListNode nn=pq.remove();//remove
-            
-            prev.next=nn;
-            prev=prev.next;
-            nn=nn.next;//update
-            
-            if(nn!=null){
-                pq.add(nn);
+        while(c1!=null && c2!=null){
+            if(c1.val>c2.val){
+                previous.next=c2;
+                c2=c2.next;
+            }else{
+                previous.next=c1;
+                c1=c1.next;
             }
+            previous=previous.next;
         }
+        previous.next=(c1!=null)?c1:c2;
         return dummy.next;
     }
 }
