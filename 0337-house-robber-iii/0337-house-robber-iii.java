48969f8a34055c416062385aa9e280cc320238bc
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2022-12-19 00:10:46 +0530

    Time: 1 ms (84.66%), Space: 44.6 MB (41.82%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.java
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -1,4 +1,4 @@
-//FOR INTERVIEW PURPOSE USE THIS FORMAT OF CLASS 
+//FO THIS IN OA'S ONLY
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,25 +15,20 @@
  * }
  */
 class Solution {
-    class Pair{
-        int withrobbery=0;
-        int withoutrobbery=0;
-    }
     public int rob(TreeNode root) {
-        Pair ans=rob_(root);
-        return Math.max(ans.withrobbery,ans.withoutrobbery);
+        int[] ans=rob_(root);
+        return Math.max(ans[0],ans[1]);
     }
-    public Pair rob_(TreeNode node){
-        if(node==null)  return new Pair();
+    public int[] rob_(TreeNode node){
+        if(node==null)  return new int[2];
         
-        Pair leftchild=rob_(node.left);
-        Pair rightchild=rob_(node.right);
+        int[] lchild=rob_(node.left);
+        int[] rchild=rob_(node.right);
         
-        Pair myans=new Pair();
-        myans.withrobbery=node.val+leftchild.withoutrobbery+rightchild.withoutrobbery;
-        myans.withoutrobbery=Math.max(leftchild.withrobbery,leftchild.withoutrobbery)+Math.max(rightchild.withrobbery,rightchild.withoutrobbery);
+        int[] myans=new int[2];
+        myans[0]=node.val+lchild[1]+rchild[1];//doing robbery
+        myans[1]=Math.max(lchild[0],lchild[1])+Math.max(rchild[0],rchild[1]);//without robbery
         
         return myans;
-        
-    } 
+    }
 }
