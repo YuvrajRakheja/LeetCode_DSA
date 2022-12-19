5608df22ecd526420fa7d126e2a84497e09a21cf
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2022-12-20 02:07:28 +0530

    Added solution - LeetHub

---
+++ Multiply two linked lists - GFG/multiply-two-linked-lists.java
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;
+    }
+}
+
+class MultiplyTwoLL{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			Node head=null;
+			Node phead=null;
+			int n=sc.nextInt();
+			while(n-->0){
+				int n1=sc.nextInt();
+				if(head==null)
+				head=new Node(n1);
+			else{
+				addHead(head,n1);
+			}
+			}
+			int m=sc.nextInt();
+			while(m-->0){
+				int n1=sc.nextInt();
+				if(phead==null)
+				phead=new Node(n1);
+			else{
+				addPhead(phead,n1);
+			}
+			}
+		GfG g=new GfG();
+
+System.out.println(g.multiplyTwoLists(head,phead));
+		}
+	}
+    public static void addHead(Node node,int a) {
+          if (node == null)
+            return;
+		if(node.next==null)
+			node.next=new Node(a);
+		else
+			addHead(node.next,a);
+	}
+	public static void addPhead(Node node,int a){
+		 if (node == null)
+            return;
+		if(node.next==null)
+		node.next=new Node(a);
+	else
+		addPhead(node.next,a);
+	}
+	}
+	
+	
+// } Driver Code Ends
+
+
+/*Node is defined as
+class Node
+{
+    int data; 
+    Node next;
+    Node(int data) {
+        this.data=data;
+        this.next = null;
+    }
+}*/
+
+class GfG{
+  /*You are required to complete this method */
+   public long multiplyTwoLists(Node l1,Node l2){
+          //add code here.
+          long n=(int)Math.pow(10,9)+7;
+          long c1=0,c2=0;
+          
+          while(l1!=null || l2!=null){
+              if(l1!=null){
+                  c1=(c1*10)%n+l1.data;
+                  l1=l1.next;
+              }
+              if(l2!=null){
+                  c2=(c2*10)%n+l2.data;
+                  l2=l2.next;
+              }
+          }
+          return (c1*c2)%n;
+   }
+}
