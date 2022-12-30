f2d41aed7c378a64848b882d51bd5eff9aeaedb8
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2022-12-30 22:22:52 +0530

    Time: 134 ms (85.48%), Space: 76 MB (90.55%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.java
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.java
@@ -1,52 +1,39 @@
+//https://www.youtube.com/watch?v=gM6egF6_aac
+
 class Solution {
-    private class Task implements Comparable {
-        final int id;
-        final int et;
-        final int pt;
-
-        public Task(int id, int et, int pt) {
-            this.id = id;
-            this.et = et;
-            this.pt = pt;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            Task t = (Task)o;
-            return (pt == t.pt) ? Integer.compare(id, t.id) : Integer.compare(pt, t.pt);
-        }
-    }
-
-
     public int[] getOrder(int[][] tasks) {
-        List<Task> stasks = new ArrayList<>(tasks.length); 
-        for (int i = 0 ; i < tasks.length; ++i) {
-            stasks.add(new Task(i, tasks[i][0], tasks[i][1]));
+        
+        int[][] alltasks=new int[tasks.length][3];
+        
+        for(int i=0;i<tasks.length;i++){
+            alltasks[i][0]=i;//index;
+            alltasks[i][1]=tasks[i][0];//time when available
+            alltasks[i][2]=tasks[i][1];//processing time
         }
-        stasks.sort((t1, t2) -> Integer.compare(t1.et, t2.et));
+        Queue<int[]> minheap = new PriorityQueue<>((a, b) -> a[2] - b[2] == 0 ? a[0] - b[0] : a[2] - b[2]);//minheap on basis of shortest processing time,if same then give with lower index
+        Arrays.sort(alltasks,(a,b)-> a[1]-b[1]);//sort alltasks on basis of time availability
         
-        PriorityQueue<Task> pq = new PriorityQueue<>();
+        int i=0,index=0,curTime=0;
+        int[] result=new int[tasks.length];
         
-        int i = 0;
-        int t = 0;
-        int j = 0;
-        int[] ans = new int[tasks.length];
         
-        while (i < stasks.size() || !pq.isEmpty()) {
-            if (i < stasks.size() && pq.isEmpty() && t < stasks.get(i).et) {
-                t = stasks.get(i).et;
+        while(i<tasks.length){
+            while(i<tasks.length && curTime>=alltasks[i][1]){
+                minheap.add(alltasks[i++]);
             }
-            while (i < stasks.size() && stasks.get(i).et <= t) {
-                pq.add(stasks.get(i));
-                i += 1;
+            if(minheap.isEmpty()){
+                curTime=alltasks[i][1];
             }
-            if (!pq.isEmpty()) {
-                ans[j] = pq.peek().id;
-                j += 1;
-                t += pq.poll().pt;
+            if(!minheap.isEmpty()){
+                int[] removed=minheap.poll();
+                result[index++]=removed[0];//storing index of result
+                curTime+=removed[2];//adding processing time
             }
         }
-        return ans;
+        while(!minheap.isEmpty()){
+            int[] removed=minheap.poll();//difference in poll and remove is that poll gives null if empty and remove return null if empty
+            result[index++]=removed[0];//storing index of result
+        }
+        return result;
     }
 }
-
