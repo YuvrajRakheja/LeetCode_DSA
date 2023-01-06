f1e0e708a2bb7c853e68fca4db3404746e5323c2
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-01-07 00:33:06 +0530

    Time: 6 ms (98.82%), Space: 60.2 MB (55.45%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,19 +1,33 @@
-//TC-0(NLOGN)
-//SC-0(1)
-//BRUTE FORCE
+//TC-0(N)
+//SC-0(N)
+//OPTIMIZED
+//https://www.youtube.com/watch?v=LxPWNln-LQY
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-        int cream=0;
+        int[] fp=new int[100001];//frequency map of costs whose range 0 to 10^5
         
-        for(int i=0;i<=costs.length-1;i++){
-             if(coins>=costs[i]){
-                 cream++;
-                 coins-=costs[i];
-             }else{
-                 break;
-             }
+        for(int i=0;i<costs.length;i++){
+            fp[costs[i]]++;
         }
-        return cream;
+        //we have stored frequency in array
+        
+        int i=1;//as i=0 is always 0 as costs[i]>=1
+        int count=0;
+        
+        while(i<100001 && coins>=i){
+            if((long)fp[i]*i>coins && coins>=i){//if particular cost coins greater than money we have to buy all coins of that type
+                int bars=coins/i;//caluclate maximum nos of coins possible of given type
+                coins-=bars*i;
+                count+=bars;
+                break;
+            }
+            if(fp[i]!=0 && coins>=i){
+                coins-=fp[i]*i;
+                count+=fp[i];
+            }
+            i++;
+        }
+        return count;
+        
     }
 }
