65274522333283bdb4d78553a83157d934c53932
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-01-15 23:02:33 +0530

    Time: 173 ms (58.69%), Space: 58.3 MB (66.89%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,37 +1,28 @@
 //TC-0(N)
 //SC-0(N)
-//https://www.youtube.com/watch?v=X0X6G-eWgQ8
+//MOST OPTIMIZED APPROACH EARLIER TAKING 2 ITERATIONS HERE IN SINGLE ONLY
+//https://www.youtube.com/watch?v=41VuLYR0btE&list=PL-Jc9J83PIiE1_SifBEWRsD-fzxrvkja9&index=6
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] rse=new int[heights.length];
-        
         Stack<Integer> st=new Stack<>();
-        for(int i=heights.length-1;i>=0;i--){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            
-            rse[i]=(st.size()>0)?st.peek():heights.length;
-            
-            st.push(i);
-        }
-        
-        st=new Stack<>();
-        int[] lse=new int[heights.length];
-        for(int i=0;i<heights.length;i++){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            
-            lse[i]=(st.size()>0)?st.peek():-1;
-            
-            st.push(i);
-        }
         
         int area=0;
-        for(int i=0;i<heights.length;i++){
-            area=Math.max(area,(rse[i]-lse[i]-1)*heights[i]);
+        
+        for(int i=0;i<=heights.length;i++){//till <=n also include last index so as to clear 
+            while(!st.isEmpty() && ((i==heights.length)||heights[st.peek()]>=heights[i])){
+                int element_under_consideration=heights[st.peek()];
+                st.pop();
+                int width;
+                if(st.isEmpty())    width=i;//rightsmall=i,leftsmall=-1 so width=i-(-1)-1=i
+                else{
+                    width=i-st.peek()-1;//rightsmall-leftsmall-1
+                }
+                
+                area=Math.max(area,element_under_consideration*width);
+            }
+            st.push(i);
         }
         return area;
+        
     }
 }
