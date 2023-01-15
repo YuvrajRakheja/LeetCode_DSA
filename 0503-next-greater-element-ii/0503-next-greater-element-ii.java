30254e16d2e69da2ce98deb0ba854b358fef1e3c
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-01-15 16:14:58 +0530

    Time: 40 ms (51.95%), Space: 43.6 MB (78.79%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,26 +1,21 @@
 //TC-0(N)
 //SC-0(N)
+
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int[] nge=new int[nums.length];
-        
         Stack<Integer> st=new Stack<>();
+        int[] nge=new int[nums.length];
+        int n=nums.length;
         
-        for(int i=nums.length-1;i>=0;i--){
-            while(st.size()>0 && nums[i]>=st.peek()){
+        //ASSUME WE ARE PUTTING NUMS ARRAY IN FRONT OF NUMS SO IT CAN BE THOUGHT CIRCULAR
+        for(int i=2*n-1;i>=0;i--){
+            while(st.size()>0 && nums[i%n]>=st.peek()){
                 st.pop();
             }
-            st.push(nums[i]);
-        }
-        
-        for(int i=nums.length-1;i>=0;i--){
-            while(!st.isEmpty() && nums[i]>=st.peek()){
-                st.pop();
+            if(i<n){
+                nge[i]=(st.size()>0)?st.peek():-1;
             }
-            
-            nge[i]=(st.size()>0)?st.peek():-1;
-            
-            st.push(nums[i]);
+            st.push(nums[i%n]);
         }
         return nge;
     }
