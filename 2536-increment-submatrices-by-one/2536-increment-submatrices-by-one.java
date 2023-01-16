0f5e308e50254291ca8ca9c7c2dfce6b493dbe04
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-01-16 16:37:08 +0530

    Time: 21 ms (100.00%), Space: 50.9 MB (83.33%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
@@ -1,18 +1,23 @@
+//TC-0(QUERIES*ROW)+0(ROW*COLUMN)=0(QUERIES*LENGTH)
+//optimized
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
         int[][] ans=new int[n][n];
         
-        for(int i=0;i<queries.length;i++){
-            int strt_row=queries[i][0];
-            int strt_col=queries[i][1];
-            int end_row=queries[i][2];
-            int end_col=queries[i][3];
+        for(int i=0;i<queries.length;i++){//IT WILL RUN AT MAX 10^4*500
+            int row_start=queries[i][0],row_end=queries[i][2];
+            int col_start=queries[i][1],col_end=queries[i][3];
             
-            for(int j=strt_row;j<=end_row;j++){
-                for(int k=strt_col;k<=end_col;k++){
-                    ans[j][k]++;
+            for(int j=row_start;j<=row_end;j++){
+                ans[j][col_start]+=1;
+                if(col_end+1<ans[0].length) ans[j][col_end+1]-=1;
+            }//here we have marked on exact match +1 and after it end -1 so that when we calculate prefix sum it is adjusted           
+        }
+        
+        for(int i=0;i<ans.length;i++){//IT WILL RUN AT MAX 500*500
+                for(int j=1;j<ans[0].length;j++){
+                    ans[i][j]+=ans[i][j-1];
                 }
-            }
         }
         return ans;
     }
