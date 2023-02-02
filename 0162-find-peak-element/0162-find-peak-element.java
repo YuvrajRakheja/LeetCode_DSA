f1175455cc9693124dd49a80e4daa2845a0b1d44
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-02-03 00:17:52 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (86.18%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,15 +1,25 @@
+//TC-0(LOGN)
+//SC-0(1)
 class Solution {
     public int findPeakElement(int[] nums) {
-        boolean flag=true;
-        int i;
+        if(nums.length==1)  return 0;
         
-        for( i=0;i<nums.length-1;i++){
-            if(nums[i]<nums[i+1]){
-                flag=false;
-                continue;
+        if(nums[0]>nums[1]) return 0;//as we r finding mid+1 and mid-1 so to prevent problem arousing on edges we make this edge cases
+        if(nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
+        
+        int low=1;//as we have checked for first case
+        int high=nums.length-2;//as we have checked for last case
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])  return mid;//mid index is peak
+            else if(nums[mid]>nums[mid+1]){//peak on left side
+                high=mid-1;
+            }else if(nums[mid]<nums[mid+1]){//peak on right side
+                low=mid+1;
             }
-            if(flag==false) return i;            
         }
-        return flag==false?i:0;
+        return -1;
     }
 }
