1b4cc3472f5669272e3ac1b718d7c10ca416ceee
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-03-16 01:54:18 +0530

    Time: 4 ms (77.78%), Space: 41.1 MB (70.71%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,29 @@
+//TC-0(2N)
+//SC-0(1)
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        if(s==null || s.length()==0)    return null;
+        
+        List<Integer> ans=new ArrayList<>();
+        
+        int[] lastindex=new int[26];
+        
+        for(int i=0;i<s.length();i++){
+            lastindex[s.charAt(i)-'a']=i;//filling of impact of characters
+        }
+        
+        int maxindx=0;
+        int initial=-1;
+        
+        //making of result
+        for(int i=0;i<s.length();i++){
+            maxindx=Math.max(lastindex[s.charAt(i)-'a'],maxindx);
+            
+            if(maxindx==i){
+                ans.add(i-initial);
+                initial=i;
+            }
+        }
+        return ans;
+    }
+}
