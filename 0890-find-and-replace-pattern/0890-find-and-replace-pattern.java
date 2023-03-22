02bcfbcd67736711aa1243a02b4cd034f8402d5b
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-03-22 17:20:24 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (80.37%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -10,24 +10,9 @@ class Solution {
         return ans;
     }
     public boolean isMatching(String word,String pattern){
-        HashMap<Character,Character> map=new HashMap<>();//mapping word char and pattern chars
-        HashSet<Character> set=new HashSet<>();//pattern ka hai
-        
-        
         for(int i=0;i<word.length();i++){
-            char w=word.charAt(i);
-            char p=pattern.charAt(i);
-            
-            if(map.containsKey(w)){
-                //pattern character already mapped
-                if(map.get(w)!=p)   return false;
-            }else{
-                //pattern character is new for mapping
-                if(set.contains(p)){
-                    return false; //matlab new w ka jo p hai woh already hai toh condition violate.Dry run for word-abc  pattern-rpr
-                }
-                set.add(p);
-                map.put(w,p);
+            if(word.indexOf(word.charAt(i))!=pattern.indexOf(pattern.charAt(i))){//The indexOf() method returns the position of the first occurrence of specified character(s) in a string.So visualize dry run for word-xyy pattern-abb   word me x-1,y-2 and pattern me a-1,b-2
+                return false;
             }
         }
         return true;
