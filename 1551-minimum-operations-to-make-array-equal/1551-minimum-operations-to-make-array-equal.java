0d252989eb656928ddb274e2cc976ac0223613ec
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-01 23:42:36 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (35.30%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,14 +1,27 @@
-//TC-0(N)
+//TC-0(1)
 //SC-0(1)
 class Solution {
     public int minOperations(int n) {
-        int ans=0;
-        // N=4   1 3 5 7
+        //Explaination
+        // N=4   1 3 5 7 N IS EVEN
         //MEAN=4
         //DIFFERENCE IS -3 -1 +1 +3
-        //SO OPERATIONS TO BE CARRIED IS 3+1=4
-        for(int i=1;i<n;i+=2){
-            ans+=n-i;
+        
+        //do n/2 as half is positive and half is negative
+        
+        //SO OPERATIONS TO BE CARRIED IS 1+3=4....IN GENERAL IT IS N/2(2A +(N-1)D)==N/2(2+(N-1)2)=N/2(2N)=N^2
+        //IN ODD CASE
+        //N=5  1 3 5 7 9  MEAN=5
+        //DIFFERENCE IS -4 -2  0 +2 +4
+        //SO OPERATIONS TO BE CARRIED IS 2+4=6....   
+        //IN GENERAL IT IS SUM IS =N/2(2A+(N-1)D)=N/2(4+(N-1)2)=N/2(2N+2)=N(N+1)
+        int ans=0;
+        if(n%2==1){//odd case
+            n=n/2;
+            ans=n*(n+1);
+        }else{//even case
+            n=n/2;
+            ans=n*n;
         }
         return ans;
     }
