18036725d4d48856ccf02f7c6d81ee4317089424
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-10 03:08:10 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (34.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,16 @@
 //TC-0(N)
-//SC-0(n)
-//Tabulation
+//SC-0(1)
+//Space optimization
 class Solution {
     public int climbStairs(int n) {
-        int [] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
+        int a=1;
+        int b=1;
         
         for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int c=a+b;
+            a=b;
+            b=c;
         }
-        return dp[n];        
+        return b;        
     }
 }
