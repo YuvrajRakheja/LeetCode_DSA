52b454038bbef3f74bff01a0ca2b8cb04dfd86e3
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-17 02:03:03 +0530

    Time: 38 ms (21.36%), Space: 51.7 MB (7.82%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,19 +1,26 @@
+//TC-0(M*N)
+//SC-0(M*N)+0(M+N)//first is dp space and second is auxilliary stack space
+//Memoization
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int[][] dp=new int[text1.length()+1][text2.length()+1];
-        
-        for(int i=1;i<=dp.length-1;i++){
-            for(int j=1;j<=dp[0].length-1;j++){
-                char c1=text1.charAt(i-1);
-                char c2=text2.charAt(j-1);
-                
-                if(c1==c2){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
+        int[][] dp=new int[text1.length()][text2.length()];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                dp[i][j]=-1;
             }
         }
-        return dp[dp.length-1][dp[0].length-1];
+        return f(text1.length()-1,text1,text2.length()-1,text2,dp);
+    }
+    
+    public int f(int idx1,String text1,int idx2,String text2,int[][] dp){
+        if(idx1<0||idx2<0)    return 0;
+        
+        if(dp[idx1][idx2]!=-1)  return dp[idx1][idx2];
+        
+        if(text1.charAt(idx1)==text2.charAt(idx2)){
+            return dp[idx1][idx2]=1+f(idx1-1,text1,idx2-1,text2,dp);
+        }else{
+            return dp[idx1][idx2]=(int)Math.max(f(idx1-1,text1,idx2,text2,dp),f(idx1,text1,idx2-1,text2,dp));
+        }
     }
 }
