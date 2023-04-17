921e77c31abe74338cfc9d6ada6c94f30bdf7bdd
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-18 01:09:16 +0530

    Time: 3 ms (27.89%), Space: 44.6 MB (98.88%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,32 +1,34 @@
 //TC-0(M*N) 
-//SC-0(M*N) DP SPACE
-//Tabulation
+//SC-0(M) DP SPACE
+//Tabulation Space Optimized
 class Solution {
     public int minPathSum(int[][] grid) {
-        int[][] dp=new int[grid.length][grid[0].length];
+        int[] curr=new int[grid[0].length];
+        int[] prev=new int[grid[0].length];
         
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
                 if(i==0 && j==0){
-                    dp[i][j]=grid[0][0];
+                    curr[j]=grid[0][0];
                 }else{
                     int right=grid[i][j];
                     if(j>0){
-                        right+=dp[i][j-1];
+                        right+=curr[j-1];
                     }else{//negative base cases
                         right+=(int)Math.pow(10,9);
                     }
                     
                     int down=grid[i][j];
                     if(i>0){
-                        down+=dp[i-1][j];
+                        down+=prev[j];
                     }else{//negative base cases
                         down+=(int)Math.pow(10,9);
                     }
-                    dp[i][j]=Math.min(right,down);
+                    curr[j]=Math.min(right,down);
                 }
             }
+            prev=curr;
         }
-        return dp[grid.length-1][grid[0].length-1];
+        return prev[grid[0].length-1];
     }
 }
