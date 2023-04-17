b0b0ffc4200b322fead1d4477a8134bf75f2be7a
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-17 21:14:51 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (97.16%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,26 +1,28 @@
 //TC-0(M*N)
-//SC-0(M*N) DP SPACE
-//TABULATION
+//SC-0(M) DP SPACE
+//TABULATION SPACE OPTIMIZED
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         
-        int[][] dp=new int[m][n];
+        int[] curr=new int[n];
+        int[] prev=new int[n];
         
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(obstacleGrid[i][j]==1){
-                    dp[i][j]=0;
+                    curr[j]=0;
                 }else if(i==0 && j==0){
-                    dp[i][j]=1;
+                    curr[j]=1;
                 }else{
-                    int up=(i>0)?dp[i-1][j]:0;
-                    int down=(j>0)?dp[i][j-1]:0;
-                    dp[i][j]=up+down;
+                    int up=(i>0)?prev[j]:0;
+                    int down=(j>0)?curr[j-1]:0;
+                    curr[j]=up+down;
                 }
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
