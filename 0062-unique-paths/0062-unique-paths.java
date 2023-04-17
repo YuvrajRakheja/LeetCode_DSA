2b61a32aec556abe1703b5ede22b4e74c322eaa5
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-17 20:29:33 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (76.99%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,21 @@
 //TC-0(M*N)
-//SC-0(M*N)
-//TABULATION
+//SC-0(M)
+//TABULATION SPACE OPTIMIZED
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        dp[0][0]=1;
+        int[] curr=new int[n];
+        int[] prev=new int[n];
         
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(i==0 && j==0){
-                    dp[i][j]=1;
+                    curr[j]=1;
                 }else{
-                    dp[i][j]=((i>0)?dp[i-1][j]:0)+((j>0)?dp[i][j-1]:0);
+                    curr[j]=((i>0)?prev[j]:0)+((j>0)?curr[j-1]:0);
                 }
             }
+            prev=curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 }
