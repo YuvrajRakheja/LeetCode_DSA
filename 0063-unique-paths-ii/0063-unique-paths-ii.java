74426c2a9e8a1cd2ebc523579f38423badd9e125
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-17 21:12:46 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (40.36%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,28 +1,26 @@
 //TC-0(M*N)
-//SC-0(M*N)+0(M+N) DP SPACE+RECURSION STACK SPACE
-//MEMOIZATION
+//SC-0(M*N) DP SPACE
+//TABULATION
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
         
         int[][] dp=new int[m][n];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;
+                }else if(i==0 && j==0){
+                    dp[i][j]=1;
+                }else{
+                    int up=(i>0)?dp[i-1][j]:0;
+                    int down=(j>0)?dp[i][j-1]:0;
+                    dp[i][j]=up+down;
+                }
             }
         }
-        return uniquepath(obstacleGrid.length-1,obstacleGrid[0].length-1,obstacleGrid,dp);
-    }
-    public int uniquepath(int m,int n,int[][] grid,int[][] dp){
-        if(m>=0 && n>=0 && grid[m][n]==1)   return 0;//obstacle
-        if(m==0 && n==0)    return 1;
-        if(m<0 || n<0)  return 0;
-        if(dp[m][n]!=-1)    return dp[m][n];//memoization
-        
-        int left=uniquepath(m,n-1,grid,dp);
-        int right=uniquepath(m-1,n,grid,dp);
-        
-        return dp[m][n]=left+right;
+        return dp[m-1][n-1];
     }
 }
