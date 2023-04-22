1883192e6910d0ea16840a3bb2124a1e3faa1d27
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-23 04:42:01 +0530

    Time: 5 ms (43.23%), Space: 42.3 MB (62.41%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,26 +1,21 @@
-//TC-0(N*N) //BRUTE FORCE IS TC-0(2^(N^2)) KAAFI JYADA IN RECURSION
-//SC-0(N*N+N) 
-//MEMOIZED 
+//TC-0(N*N) 
+//SC-0(N*N) 
+//TABULATION 
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int[][] dp=new int[triangle.size()][triangle.size()];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp.length;j++){
-                dp[i][j]=-1;
+        int row=triangle.size(),col=triangle.size();
+        
+        for(int j=0;j<col;j++){
+            dp[row-1][j]=triangle.get(row-1).get(j);
+        }
+        for(int i=row-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=triangle.get(i).get(j)+dp[i+1][j];
+                int down_right=triangle.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(down,down_right);
             }
         }
-        return helper(0,0,triangle,dp);
-    }
-    public int helper(int row,int col,List<List<Integer>> triangle,int[][] dp){
-        if(row==triangle.size()-1){
-            return triangle.get(row).get(col);
-        }
-        if(dp[row][col]!=-1){
-            return dp[row][col];
-        }
-        int down=triangle.get(row).get(col)+helper(row+1,col,triangle,dp);
-        int down_right=triangle.get(row).get(col)+helper(row+1,col+1,triangle,dp);
-        
-        return dp[row][col]=(int)Math.min(down,down_right);
+        return dp[0][0];
     }
 }
