7822e4094853c2cd6c150453b4b284d6a7ba7076
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-23 03:37:45 +0530

    Time: 2 ms (83.35%), Space: 42.7 MB (21.99%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,11 +1,13 @@
-//TC-0(N^N)
-//SC-0(n*n)+0(N)
-//MEMOIZATION
+//TC-0(N*N) //BRUTE FORCE IS TC-0(2^(N^2)) KAAFI JYADA IN RECURSION
+//SC-0(N*N+N) 
+//MEMOIZED 
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int[][] dp=new int[triangle.size()][triangle.size()];
         for(int i=0;i<dp.length;i++){
-            Arrays.fill(dp[i],-1);
+            for(int j=0;j<dp.length;j++){
+                dp[i][j]=-1;
+            }
         }
         return helper(0,0,triangle,dp);
     }
@@ -17,8 +19,8 @@ class Solution {
             return dp[row][col];
         }
         int down=triangle.get(row).get(col)+helper(row+1,col,triangle,dp);
-        int up=triangle.get(row).get(col)+helper(row+1,col+1,triangle,dp);
+        int down_right=triangle.get(row).get(col)+helper(row+1,col+1,triangle,dp);
         
-        return dp[row][col]=(int)Math.min(up,down);
+        return dp[row][col]=(int)Math.min(down,down_right);
     }
 }
