397696ad2fc8cac4bed8ed57b50f0297ee0bfd82
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-23 22:36:22 +0530

    Time: 5 ms (56.35%), Space: 43 MB (79.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,40 +1,43 @@
-//TC-0(N*N)
-//SC-0(N*N)
-//TABULATION
+//TC-0(N*N+N)
+//SC-0(N)
+//SPACE OPTIMIZATION
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int[][] dp=new int[matrix.length][matrix[0].length];
+        int n=matrix.length;
         
-        for(int i=0;i<matrix.length;i++){
-            dp[0][i]=matrix[0][i];
-        }//BASE CASE
+        int[] prev=new int[n];
+        int[] curr=new int[n];  
         
-        for(int i=1;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                int up=matrix[i][j]+dp[i-1][j];
-                int upleft=matrix[i][j];
+        for(int i=0;i<n;i++){
+            prev[i]=matrix[0][i];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int up=matrix[i][j]+prev[j];
                 
+                int upleft=matrix[i][j];
                 if(j-1>=0){
-                    upleft+=dp[i-1][j-1];
+                    upleft+=prev[j-1];
                 }else{
                     upleft+=Math.pow(10,9);
                 }
                 
                 int upright=matrix[i][j];
-                
                 if(j+1<matrix.length){
-                    upright+=dp[i-1][j+1];
+                    upright+=prev[j+1];
                 }else{
                     upright+=Math.pow(10,9);
                 }
-                
-                dp[i][j]=Math.min(up,Math.min(upleft,upright));
+                curr[j]=Math.min(up,Math.min(upleft,upright));
             }
+            prev=curr;
+            curr=new int[n];
         }
-        int min=Integer.MAX_VALUE;
-        for(int i=0;i<matrix.length;i++){
-            min=Math.min(min,dp[matrix.length-1][i]);
+        int mini=Integer.MAX_VALUE;
+        
+        for(int i=0;i<n;i++){
+            mini=Math.min(mini,prev[i]);
         }
-        return min;
+        return mini;
     }
 }
