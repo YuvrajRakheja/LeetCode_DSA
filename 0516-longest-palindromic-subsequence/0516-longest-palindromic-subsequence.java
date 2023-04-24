789a9ae737d0707240c2e5aefcdc37b1d03d2378
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-25 02:58:59 +0530

    Time: 98 ms (14.66%), Space: 42.5 MB (94.74%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,6 +1,6 @@
 //TC-0(M*N)+0(N)FOR REVERSING
 //SC-0(M*N)+0(M) A.STACK SPACE
-//TABULATION
+//SPACE OPTIMIZED
 public class Solution {
     public int longestPalindromeSubseq(String s) {
         StringBuilder sb=new StringBuilder();
@@ -11,24 +11,24 @@ public class Solution {
         return lcs(s,sb.toString());
     }
     public int lcs(String s1,String s2){
-        int[][] dp=new int[s1.length()+1][s2.length()+1];
+        int[] prev=new int[s2.length()+1];
+        int[] curr=new int[s2.length()+1];
         
-        for(int i=0;i<s1.length();i++){
-            dp[i][0]=0;    
-        }
         for(int j=0;j<s2.length();j++){
-            dp[0][j]=0;    
+            prev[j]=0;    
         }//negative base case of recurssion
         
         for(int i=1;i<=s1.length();i++){
             for(int j=1;j<=s2.length();j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 }else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=Math.max(prev[j],curr[j-1]);
                 }
             }
+            prev=curr;
+            curr=new int[s2.length()+1];
         }
-        return dp[s1.length()][s2.length()];
+        return prev[s2.length()];
     }
 }
