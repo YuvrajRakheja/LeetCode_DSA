ff53e1ef7b5c67aefab18851d80730e1dd49f718
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-25 14:48:04 +0530

    Time: 22 ms (30.41%), Space: 49.6 MB (64.76%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,35 +1,26 @@
 //TC-0(M*N)
 //SC-0(M*N)+0(M+N)
-//MEMOIZATION
+//TABULATION
 
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp=new int[s.length()][t.length()];
-        for(int i=0;i<s.length();i++){
-            for(int j=0;j<t.length();j++){
-                dp[i][j]=-1;
+        int[][] dp=new int[s.length()+1][t.length()+1];
+        for(int i=0;i<=s.length();i++){
+            dp[i][0]=1;
+        }
+        for(int j=1;j<=t.length();j++){
+            dp[0][j]=0;
+        }//negative base cases
+        
+        for(int i=1;i<=s.length();i++){
+            for(int j=1;j<=t.length();j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
             }
         }
-        
-        return helper(s.length()-1,s,t.length()-1,t,dp);
-    }
-    public int helper(int i,String s,int j,String t,int[][] dp){
-        if(j<0){
-            return 1;
-        }
-        if(i<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s.charAt(i)==t.charAt(j)){
-            int take=helper(i-1,s,j-1,t,dp);
-            int nottake=helper(i-1,s,j,t,dp);
-            
-            return dp[i][j]=take+nottake;
-        }else{
-            return dp[i][j]=helper(i-1,s,j,t,dp);
-        }
+        return dp[s.length()][t.length()];
     }
 }
