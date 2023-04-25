c016520071fa6c75a3e66415c430abc281ed72a0
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-25 15:03:57 +0530

    Time: 19 ms (72.89%), Space: 42.8 MB (87.98%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,26 +1,33 @@
 //TC-0(M*N)
-//SC-0(M*N)+0(M+N)
-//TABULATION
+//SC-0(N)
+//SPACE OPTIMIZATION
 
 class Solution {
     public int numDistinct(String s, String t) {
-        int[][] dp=new int[s.length()+1][t.length()+1];
-        for(int i=0;i<=s.length();i++){
-            dp[i][0]=1;
-        }
+        int[] prev=new int[t.length()+1];
+        int[] curr=new int[t.length()+1];
+        
+        // for(int i=0;i<=s.length();i++){
+        //     dp[i][0]=1;
+        // }
+        prev[0]=curr[0]=1;
+        
         for(int j=1;j<=t.length();j++){
-            dp[0][j]=0;
+            prev[j]=0;
         }//negative base cases
         
         for(int i=1;i<=s.length();i++){
             for(int j=1;j<=t.length();j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j]=(dp[i-1][j-1]+dp[i-1][j]);
+                    curr[j]=(prev[j-1]+prev[j]);
                 }else{
-                    dp[i][j]=dp[i-1][j];
+                    curr[j]=prev[j];
                 }
             }
+            prev=curr;
+            curr=new int[t.length()+1];
+            curr[0]=1;
         }
-        return dp[s.length()][t.length()];
+        return prev[t.length()];
     }
 }
