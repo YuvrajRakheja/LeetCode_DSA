0682ff24b5e860773a92783f0cb64e3b0a5bd876
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-26 03:47:37 +0530

    Time: 6 ms (71.80%), Space: 43.1 MB (6.55%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,28 +1,30 @@
 //TC-0(M*N)
-//SC-0(M*N)
-//TABULATION
+//SC-0(N)
+//SPACE OPTIMIZATION
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp=new int[word1.length()+1][word2.length()+1];
+        int[] prev=new int[word2.length()+1];
+        int[] curr=new int[word2.length()+1];
         //dp of 1 size greater to incorporate negative base cases
         
-        for(int i=0;i<=word1.length();i++){
-            dp[i][0]=i;    
-        }    
         for(int j=0;j<=word2.length();j++){
-            dp[0][j]=j;    
+            prev[j]=j;    
         }
     
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
+        for(int i=1;i<=word1.length();i++){
+            curr[0]=i;
+            
+            for(int j=1;j<=word2.length();j++){
                 if(word1.charAt(i-1)==word2.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }else{
-                    dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+                    curr[j]=1+Math.min(curr[j-1],Math.min(prev[j],prev[j-1]));
                 }
             }
+            prev=curr;
+            curr=new int[word2.length()+1];
         }
         
-        return dp[word1.length()][word2.length()];            
+        return prev[word2.length()];            
     }
 }
