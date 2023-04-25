adce4c773ae81ee7d8dceb7636085b760d102182
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-26 03:40:51 +0530

    Time: 6 ms (71.80%), Space: 42.3 MB (93.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,29 +1,28 @@
 //TC-0(M*N)
-//SC-0(M*N)+0(M+N)
+//SC-0(M*N)
 //TABULATION
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp=new int[word1.length()][word2.length()];
+        int[][] dp=new int[word1.length()+1][word2.length()+1];
+        //dp of 1 size greater to incorporate negative base cases
         
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
+        for(int i=0;i<=word1.length();i++){
+            dp[i][0]=i;    
+        }    
+        for(int j=0;j<=word2.length();j++){
+            dp[0][j]=j;    
+        }
+    
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]));
+                }
             }
         }
         
-        return helper(word1.length()-1,word1,word2.length()-1,word2,dp);
-    }
-    public int helper(int i,String word1,int j,String word2,int[][] dp){
-        if(i<0) return j+1;//if word1 is finish we have to insert all chars in word2
-        if(j<0) return i+1;//for deleting all left in word1 as our word is formed
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(word1.charAt(i)==word2.charAt(j)){
-            return dp[i][j]=0+helper(i-1,word1,j-1,word2,dp);
-        }else{
-            return dp[i][j]=(int)1+Math.min(helper(i,word1,j-1,word2,dp),Math.min(helper(i-1,word1,j,word2,dp),helper(i-1,word1,j-1,word2,dp)));//1 st is insert 2 nd is delete 3rd is replace
-        }            
+        return dp[word1.length()][word2.length()];            
     }
 }
