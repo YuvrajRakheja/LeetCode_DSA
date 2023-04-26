96ec820bea45f6f82c946fea36d998762ac7c4fd
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-27 04:27:51 +0530

    Time: 11 ms (47.29%), Space: 57.7 MB (71.08%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,18 +1,14 @@
-//TC-0(N*N)
-//SC-0(N*3*2)
-//Tabulation
+//TC-0(N*2*3)
+//SC-0(3*2)--0(1)
+//SPACE OPTIMIZATION
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][][] dp=new int[prices.length+1][2][3];//prices n,2 buy or sell 3 (0,1,2)transaction
-        
-        for(int i=0;i<prices.length;i++){
-            for(int j=0;j<2;j++){
-                dp[i][j][0]=0;//capacity ==0 return 0 wala base case
-            }
-        }
+        int[][] after=new int[2][3];//2 buy or sell 3 (1,2)transaction
+        int[][] curr=new int[2][3];//2 buy or sell 3 (1,2)transaction
+                
         for(int j=0;j<2;j++){
             for(int k=0;k<3;k++){
-                dp[prices.length][j][k]=0;//idx==n wala base case
+                after[j][k]=0;//idx==n wala base case
             }
         }
         
@@ -20,13 +16,14 @@ class Solution {
             for(int buy=0;buy<2;buy++){
                 for(int cap=1;cap<3;cap++){
                     if(buy==1){
-                        dp[i][buy][cap]=Math.max(-prices[i]+dp[i+1][0][cap],0+dp[i+1][1][cap]);
+                        curr[buy][cap]=Math.max(-prices[i]+after[0][cap],0+after[1][cap]);
                     }else{
-                        dp[i][buy][cap]=Math.max(prices[i]+dp[i+1][1][cap-1],dp[i+1][0][cap]);
+                        curr[buy][cap]=Math.max(prices[i]+after[1][cap-1],after[0][cap]);
                     }
                 }
             }
+            after=curr;
         }
-        return dp[0][1][2];        
+        return after[1][2];        
     }
 }
