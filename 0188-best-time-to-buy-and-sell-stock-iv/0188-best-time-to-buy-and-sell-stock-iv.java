6965a9e058c80d7f44896607782721d1308a4c99
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-27 17:32:27 +0530

    Time: 2 ms (88.12%), Space: 41 MB (80.66%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,26 +1,25 @@
 //TC-0(N*2*K)
 //SC-0(N*2*K)
-//Tabulation
+//Space Optimization
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int[][] dp=new int[prices.length+1][k*2+1];
-        
+        int[] after=new int[2*k+1];
+        int[] curr=new int[2*k+1];
+    
         for(int i=0;i<2*k;i++){
-            dp[prices.length][i]=0;//idx==prices.length return 0;
-        }
-        for(int i=0;i<prices.length;i++){
-            dp[i][2*k]=0;
-        }
+            after[i]=0;//idx==prices.length return 0;
+        }    
         
         for(int i=prices.length-1;i>=0;i--){
             for(int tran_no=2*k-1;tran_no>=0;tran_no--){
                 if(tran_no%2==0){//buy case
-                    dp[i][tran_no]=(int)Math.max(-prices[i]+dp[i+1][tran_no+1],0+dp[i+1][tran_no]);//first is we bought share and second we didnt buy 
+                    curr[tran_no]=(int)Math.max(-prices[i]+after[tran_no+1],0+after[tran_no]);//first is we bought share and second we didnt buy 
                 }else{//sell case
-                    dp[i][tran_no]=(int)Math.max(prices[i]+dp[i+1][tran_no+1],0+dp[i+1][tran_no]);//first is sell case and second we didnt sell
+                    curr[tran_no]=(int)Math.max(prices[i]+after[tran_no+1],0+after[tran_no]);//first is sell case and second we didnt sell
                 }
             }
+            after=curr;
         }
-        return dp[0][0];
+        return after[0];
     }
 }
