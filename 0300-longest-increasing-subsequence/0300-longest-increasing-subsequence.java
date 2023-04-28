39aa4f8a6387e89dc7122b84d720dbf7bb68088a
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-29 04:24:10 +0530

    Time: 155 ms (12.87%), Space: 110.5 MB (11.78%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,27 @@
 //TC-0(n*n)
-//SC-0(n*n)+0(n)
-//Memoization
+//SC-0(n*n)
+//Tabulation
+
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp=new int[nums.length][nums.length+1];
+        int[][] dp=new int[nums.length+1][nums.length+1];
         //since prev_index varies from -1 to nums.length-1 so total nums.length+1 and we have done coordinate change
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
+        for(int i=0;i<nums.length+1;i++){//idx array
+            dp[nums.length][i]=0;//idx=nums.length    
+        }
+        
+        for(int i=nums.length-1;i>=0;i--){
+            for(int prev_idx=i-1;prev_idx>=-1;prev_idx--){
+                int nottake=0+dp[i+1][prev_idx+1];
+                
+                int take=0;
+                if(prev_idx==-1||nums[i]>nums[prev_idx]){
+                    take=1+dp[i+1][i+1];
+                }
+                
+                dp[i][prev_idx+1]=Math.max(take,nottake);
             }
         }
-        return helper(0,-1,nums,dp);
-    }
-    public int helper(int idx,int prev_idx,int[] nums,int[][] dp){
-        if(idx==nums.length)    return 0;
-        if(dp[idx][prev_idx+1]!=-1)   return dp[idx][prev_idx+1];
-        
-        int len=0+helper(idx+1,prev_idx,nums,dp);//NOT_TAKE
-        
-        if(prev_idx+1==0 || nums[idx]>nums[prev_idx]){
-            len=Math.max(len,1+helper(idx+1,idx,nums,dp));//TAKE
-        }
-        return dp[idx][prev_idx+1]=len;
+        return dp[0][0];
     }
 }
