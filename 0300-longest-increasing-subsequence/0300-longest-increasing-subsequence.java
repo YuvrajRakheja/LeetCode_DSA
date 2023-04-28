9d880749bcb5d7cc7d39dc6bb9b37bd5a3b0f6a3
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-04-29 04:25:57 +0530

    Time: 111 ms (17.93%), Space: 43 MB (16.61%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,29 @@
 //TC-0(n*n)
 //SC-0(n*n)
-//Tabulation
+//Space Optimization
 
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp=new int[nums.length+1][nums.length+1];
-        //since prev_index varies from -1 to nums.length-1 so total nums.length+1 and we have done coordinate change
+        int[] curr=new int[nums.length+1];
+        int[] after=new int[nums.length+1];
+        
         for(int i=0;i<nums.length+1;i++){//idx array
-            dp[nums.length][i]=0;//idx=nums.length    
+            after[i]=0;//idx=nums.length    
         }
         
         for(int i=nums.length-1;i>=0;i--){
             for(int prev_idx=i-1;prev_idx>=-1;prev_idx--){
-                int nottake=0+dp[i+1][prev_idx+1];
+                int nottake=0+after[prev_idx+1];
                 
                 int take=0;
                 if(prev_idx==-1||nums[i]>nums[prev_idx]){
-                    take=1+dp[i+1][i+1];
+                    take=1+after[i+1];
                 }
                 
-                dp[i][prev_idx+1]=Math.max(take,nottake);
+                curr[prev_idx+1]=Math.max(take,nottake);
             }
+            after=(int[])curr.clone();
         }
-        return dp[0][0];
+        return after[0];
     }
 }
