7f02697195ae943850cf53b5bcf8658c7b468ce3
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-06-04 23:57:07 +0530

    Added solution - LeetHub

---
+++ Reversing the equation - GFG/reversing-the-equation.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(in.readLine().trim());
+        while(t-- > 0)
+        {
+            String s;
+            s = in.readLine().trim();
+            
+            Solution ob = new Solution();
+            
+            out.println(ob.reverseEqn(s));    
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+   
+    String reverseEqn(String s)
+    {
+        // your code here
+        int i=s.length()-1;
+        StringBuilder ans=new StringBuilder();
+        
+        while(i>=0){
+            int num=0;
+            int pow=0,j=i;
+            
+            while(j>=0 && s.charAt(j)>='0' && s.charAt(j)<='9'){
+                num+=(int)(s.charAt(j)-'0')*Math.pow(10,pow);
+                pow++;
+                j--;
+            }
+            ans.append(Integer.toString(num));
+            
+            while(j>=0 && (s.charAt(j)<'0'||s.charAt(j)>'9')){
+                ans.append(s.charAt(j));
+                j--;
+            }
+            i=j;
+        }
+        return ans.toString();
+        
+    }
+}
