d90134a6c75644ff4784cab3e5274076f693f073
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-06-16 21:56:16 +0530

    Time: 0 ms (100.00%), Space: 40 MB (98.70%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,23 +1,18 @@
-//TC-0(N+N)
+//TC-0(N)
 //SC-0(N)
-//2 QUEUES
+//1 QUEUE
 class MyStack {
     Queue<Integer> q1;
-    Queue<Integer> q2;  
     public MyStack() {
         q1=new LinkedList<>();
-        q2=new LinkedList<>();
     }
     
     public void push(int x) {
-        q2.add(x);
-        while(!q1.isEmpty()){
-               q2.add(q1.peek());
+        q1.add(x);
+        for(int i=0;i<q1.size()-1;i++){
+               q1.add(q1.peek());
                q1.remove();
-        }
-        q1=q2;
-        q2=new LinkedList<>();
-        
+        }        
     }
     
     public int pop() {
