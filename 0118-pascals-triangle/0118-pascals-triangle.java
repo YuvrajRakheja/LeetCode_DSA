a6b332178a58efa133c0dc2fa876705c8fe3e8a1
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-07-23 02:32:09 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (25.41%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -3,22 +3,24 @@ class Solution {
         List<List<Integer>> ans=new ArrayList<>();
         
         for(int row=1;row<=numRows;row++){
-            List<Integer> sans=new ArrayList<>();
-            for(int col=1;col<=row;col++){
-                sans.add(ncr(row-1,col-1));
-            }
+            List<Integer> sans=generaterow(row);
             ans.add(sans);
         }
        return ans;       
     }
-    public int ncr(int n,int r){
-        int ans=1;
-        for(int i=0;i<r;i++){
-            ans*=(n-i);
-            ans/=(i+1);
+    public List generaterow(int row){
+        List<Integer> ans=new ArrayList<>();
+        int temp=1;
+        ans.add(temp);
+        
+        for(int col=1;col<row;col++){
+            temp*=(row-col);
+            temp/=col;
+            ans.add(temp);
         }
         return ans;
     }
 }
-//Time complexity 0(n3)
+//Time complexity 0(n2)
 //Space complexity 0(1)
+//optimal
