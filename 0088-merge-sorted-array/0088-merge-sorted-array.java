f6fe5282ee1b118af9792c33575adc45dfedd400
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-07-30 01:46:33 +0530

    Time: 1 ms (36.99%), Space: 41.2 MB (78.05%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,29 @@
-//TC-0(M+N)
-//SC-0(M+N)
-//BRUTE FORCE
+//TC-0(MLOGN+NLOGN)+O(min(m,n))
+//SC-0(1)
+//OPTIMIZED 
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] ans=new int[m+n];
-        int i=0,j=0,k=0;
+        int left=m-1,right=0;
         
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                ans[k++]=nums1[i];
-                i++;
+        while(left>=0 && right<n){
+            if(nums1[left]>nums2[right]){
+                swap(nums1,left,nums2,right);
+                left--;
+                right++;
             }else{
-                ans[k++]=nums2[j];
-                j++;
+                break;
             }
         }
-        while(i<m){
-            ans[k++]=nums1[i++];
+        int j=0;
+        for(int i=m;i<nums1.length;i++){
+            nums1[i]=nums2[j];
+            j++;
         }
-        while(j<n){
-            ans[k++]=nums2[j++];
-        }
-        for(int p=0;p<ans.length;p++)   nums1[p]=ans[p];
+        Arrays.sort(nums1);
+    }
+    public void swap(int[] num1,int left,int[] num2,int right){
+        int temp=num1[left];
+        num1[left]=num2[right];
+        num2[right]=temp;
     }
 }
