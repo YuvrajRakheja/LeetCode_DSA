e35daf31baee06db1b13fd799a503ef79c43078c
Yuvraj Rakheja <83249226+YuvrajRakheja@users.noreply.github.com>
2023-08-15 04:19:51 +0530

    Time: 23 ms (85.39%), Space: 54.1 MB (91.34%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,15 +1,9 @@
-//TC
+//TC-0(NLOGN)
+//SC-0(LOGN)In Java, Arrays.sort() for primitives is implemented using a variant of the Quick Sort algorithm, which has a space complexity of LOGN
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
-        int count=0,i=0;
         
-        for(i=nums.length-1;i>=0;i--){
-            count++;
-            if(k==count){
-                break;
-            }
-        }
-        return nums[i];
+        return nums[nums.length-k];
     }
 }
